c5b158cd8b98fd5240f8018b5d1ea99e4252b4d6
fredd <freddypcarrion@gmail.com>
2022-12-20 10:41:37 -0500

    app updated

--- drilling_app.py
+++ drilling_app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import plotly_express as px
 from streamlit_option_menu import option_menu
 from PIL import Image
+from collections import namedtuple
+from math import radians, acos, asin, cos, sin, atan, degrees, sqrt
 
 # Insert an icon
 icon = Image.open("Resources/well.jpg")
@@ -84,11 +86,75 @@ def data(dataframe):
 
 def plot(dataframe):
     st.header("**Well Trajectory 3D**")
-    x = st.selectbox("Enter dispns",options=dataframe.columns)
-    y = st.selectbox("Enter dispew",options=dataframe.columns)
-    z = st.selectbox("Enter tvd",options=dataframe.columns)
+    x = st.selectbox("Enter dispns", options=dataframe.columns)
+    y = st.selectbox("Enter dispew", options=dataframe.columns)
+    z = st.selectbox("Enter tvd", options=dataframe.columns)
     fig = px.line_3d(dataframe, x, y, z)
     st.plotly_chart(fig)
+
+
+# Useful functions for directional trajectories
+# Function to calculate J well parameters
+
+Data = namedtuple("Input", "TVD KOP BUR DH")
+Output = namedtuple("Output", "R Theta TVD_EOB Md_EOB Dh_EOB Tan_len Md_total")
+
+
+def well_J(data: Data, unit="ingles") -> Output:
+    # Call input values
+    tvd = data.TVD
+    kop = data.KOP
+    bur = data.BUR
+    dh = data.DH
+    if unit == "ingles":
+        R = 5729.58 / bur
+    else:
+        R = 1718.87 / bur
+    if dh > R:
+        dc = dh - R
+    elif dh < R:
+        dc = R - dh
+    do = tvd - kop
+    doc = degrees(atan(dc / do))
+    oc = sqrt(dc**2 + do**2)
+    boc = degrees(acos(R / oc))
+    if R < dh:
+        bod = boc - doc
+    elif R > dh:
+        bod = boc + doc
+    theta = 90 - bod
+    tvd_eob = kop + abs(R * sin(radians(theta)))
+    if unit == "ingles":
+        md_eob = kop + (theta / bur) * 100
+    else:
+        md_eob = kop + (theta / bur) * 30
+    dh_eob = R - R * cos(radians(theta))
+    tan_len = sqrt(oc**2 - R**2)
+    if unit == "ingles":
+        md_total = kop + (theta / bur) * 100 + tan_len
+    else:
+        md_total = kop + (theta / bur) * 30 + tan_len
+    return Output(
+        R=R,
+        Theta=theta,
+        TVD_EOB=tvd_eob,
+        Md_EOB=md_eob,
+        Dh_EOB=dh_eob,
+        Tan_len=tan_len,
+        Md_total=md_total,
+    )
+    names = ["R", "theta", "tvd_EOB", "MD_EOB", "DH_EOB", "Length_tan", "MD_Total"]
+    for param, value in zip(names, trajectory_J):
+        if unit == "ingles":
+            if param == "theta":
+                st.success(f"{param} -> {value:.3f} degrees")
+            else:
+                st.success(f"{param} -> {value:.3f} ft")
+        else:
+            if param == "theta":
+                st.success(f"{param} -> {value:.3f} degrees")
+            else:
+                st.success(f"{param} -> {value:.3f} m")
 
 
 # Call dataframe
@@ -101,3 +167,15 @@ if options == "Data":
 
 elif options == "3D Plots":
     plot(df)
+
+elif options == "Basic Calculations":
+    st.subheader("**Select unit**")
+    units = st.selectbox("Units", ("ingles", "metric"))
+    if st.checkbox("J-well type"):
+        st.subheader("**Enter input values**")
+        kop = st.number_input("Enter kop value: ")
+        tvd = st.number_input("Enter tvd value: ")
+        dh = st.number_input("Enter dh value: ")
+        bur = st.number_input("Enter bur value")
+        st.subheader("**Show results**")
+        well_J(Data(tvd, kop, bur, dh), units)
