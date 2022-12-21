d91775ab4ab7df6a757fbd0a67c513aa228bf415
fredd <freddypcarrion@gmail.com>
2022-12-21 10:13:44 -0500

    app updated

--- drilling_app.py
+++ drilling_app.py
@@ -159,15 +159,18 @@ def well_J(data: Data, unit="ingles") -> Output:
 
 # Function to calculate parameters from a S-Type well
 Data_S = namedtuple("Input", "TVD KOP BUR DOR DH")
-Output_S = namedtuple("Output", "R1 R2 Theta TVD_EOB Md_EOB Dh_EOB Tan_len Md_SOD TVD_SOD Dh_SOD Md_total")
+Output_S = namedtuple(
+    "Output", "R1 R2 Theta TVD_EOB Md_EOB Dh_EOB Tan_len Md_SOD TVD_SOD Dh_SOD Md_total"
+)
 
-def well_S(data:Data_S, unit='ingles'):
+
+def well_S(data: Data_S, unit="ingles"):
     tvd = data.TVD
     kop = data.KOP
     bur = data.BUR
     dor = data.DOR
     dh = data.DH
-    if unit == 'ingles':
+    if unit == "ingles":
         R1 = 5729.58 / bur
         R2 = 5729.58 / dor
     else:
@@ -184,35 +187,58 @@ def well_S(data:Data_S, unit='ingles'):
     fog = degrees(asin(fg / of))
     theta = fog - foe
     tvd_eob = kop + R1 * sin(radians(theta))
-    if unit == 'ingles':
+    if unit == "ingles":
         md_eob = kop + (theta / bur) * 100
     else:
         md_eob = kop + (theta / bur) * 30
     dh_eob = R1 - abs(R1 * cos(radians(theta)))
     tan_len = sqrt(of**2 - fg**2)
-    if unit == 'ingles':
+    if unit == "ingles":
         md_sod = kop + (theta / bur) * 100 + tan_len
     else:
-         md_sod = kop + (theta / bur) * 30 + tan_len
+        md_sod = kop + (theta / bur) * 30 + tan_len
     tvd_sod = tvd_eob + tan_len * abs(cos(radians(theta)))
     dh_sod = dh_eob + abs(tan_len * sin(radians(theta)))
-    if unit == 'ingles':
+    if unit == "ingles":
         md_total = kop + (theta / bur) * 100 + tan_len + (theta / dor) * 100
     else:
         md_total = kop + (theta / bur) * 30 + tan_len + (theta / dor) * 30
 
-    output_S = Output_S(R1=R1, R2=R2, Theta=theta, TVD_EOB=tvd_eob, Md_EOB=md_eob, Dh_EOB=dh_eob, \
-                    Tan_len=tan_len, Md_SOD=md_sod, TVD_SOD=tvd_sod, Dh_SOD=dh_sod, Md_total=md_total)
+    output_S = Output_S(
+        R1=R1,
+        R2=R2,
+        Theta=theta,
+        TVD_EOB=tvd_eob,
+        Md_EOB=md_eob,
+        Dh_EOB=dh_eob,
+        Tan_len=tan_len,
+        Md_SOD=md_sod,
+        TVD_SOD=tvd_sod,
+        Dh_SOD=dh_sod,
+        Md_total=md_total,
+    )
 
-    names = ['R1', 'R2', 'theta', 'tvd_EOB', 'Md_EOB', 'Dh_EOB', 'Lengh_tan', 'Md_SOD', 'tvd_SOD', 'Dh_SOD', 'Md_Total']
-    for param , value in zip(names, output_S):
+    names = [
+        "R1",
+        "R2",
+        "theta",
+        "tvd_EOB",
+        "Md_EOB",
+        "Dh_EOB",
+        "Lengh_tan",
+        "Md_SOD",
+        "tvd_SOD",
+        "Dh_SOD",
+        "Md_Total",
+    ]
+    for param, value in zip(names, output_S):
         if unit == "ingles":
-            if param == 'theta':
+            if param == "theta":
                 st.success(f"{param} -> {value:.3f} degrees")
             else:
                 st.success(f"{param} -> {value:.3f} ft")
         else:
-            if param == 'theta':
+            if param == "theta":
                 st.success(f"{param} -> {value:.3f} degrees")
             else:
                 st.success(f"{param} -> {value:.3f} m")
